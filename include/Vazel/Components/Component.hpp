68c4dbdc923ededdaad1bce7f1254c638a103521
Heliferepo <mattisdalleau@gmail.com>
2021-11-27 23:22:28 +0400

    add isValidSignature for sign check + norm

--- include/Vazel/Components/Component.hpp
+++ include/Vazel/Components/Component.hpp
@@ -35,77 +35,80 @@ namespace vazel
      */
     using ComponentType = uint16_t;
 
+    /**
+     * @brief CompoenentExistsException is thrown when a component is already added to an entity.
+     *
+     */
     class ComponentExistsException : public std::exception
     {
-        private:
-            std::string _e;
+    private:
+        std::string _e;
 
-        public:
-            ComponentExistsException(const std::string& e);
-            const char* what() const noexcept override;
+    public:
+        /**
+         * @brief Construct a new Component Exists Exception object
+         *
+         * @param e The error message.
+         */
+        ComponentExistsException(const std::string &e);
+
+        /**
+         * @brief Get the what object.
+         *
+         * @return const char* The error message.
+         */
+        const char *what() const noexcept override;
     };
 
     class Component
     {
-        private:
-            std::any _data;
+    private:
+        std::any _data;
 
-        public:
+    public:
+        /**
+         * @brief Construct a new T object with an initial value.
+         *
+         * @tparam T The type of the component.
+         * @param data The initial value of the component.
+         */
+        template <typename T>
+        void make(T &data)
+        {
+            if (_data.has_value())
+            {
+                throw ComponentExistsException("Component::make<T>: Component is not null and you are trying to make a new one.");
+            }
+            _data = data;
+        }
+        /**
+         * @brief Destroy the _data object but not the Component class.
+         */
+        void remove(void);
 
-            /**
-             * @brief Construct a new T object
-             *
-             * @tparam T The type of the component.
-             */
-            template<typename T>
-                void make()
-                {
-                    if (_data.has_value())
-                        throw ComponentExistsException("Component is not null and you are trying to make a new one.");
-                    _data = T();
-                }
+        /**
+         * @brief Construct a new Component object
+         *
+         */
+        Component(void) = default;
 
-            /**
-             * @brief Construct a new T object with an initial value.
-             *
-             * @tparam T The type of the component.
-             * @param data The initial value of the component.
-             */
-            template<typename T>
-                void make(T data)
-                {
-                    if (_data.has_value())
-                        throw ComponentExistsException("Component is not null and you are trying to make a new one.");
-                    _data = data;
-                }
-            /**
-             * @brief Destroy the _data object but not the Component class.
-             */
-            void remove(void);
+        /**
+         * @brief Destroy the Component object
+         */
+        ~Component(void);
 
-            /**
-             * @brief Construct a new Component object
-             *
-             */
-            Component(void) = default;
+        /**
+         * @brief Wrapper around get_ref
+         * @tparam Type to use around get_ref
+         * @return T& A reference by the object return by get_reg
+         */
+        template <typename T>
+        T &get(void)
+        {
+            return std::any_cast<T &>(_data);
+        }
 
-            /**
-             * @brief Destroy the Component object
-             */
-            ~Component(void);
-
-            /**
-             * @brief Wrapper around get_ref
-             * @tparam Type to use around get_ref
-             * @return T& A reference by the object return by get_reg
-             */
-            template<typename T>
-                T& get(void)
-                {
-                    return std::any_cast<T&>(_data);
-                }
-
-            bool hasValue(void) const { return _data.has_value(); }
+        bool hasValue(void) const { return _data.has_value(); }
     };
 
     /**
@@ -114,5 +117,18 @@ namespace vazel
      *
      */
     using ComponentSignature = std::bitset<VAZEL_MAX_COMPONENTS>;
+
+    /**
+     * @brief isValidSignature is a function that checks if a signature is valid.
+     *
+     * @param signature The signature to check.
+     * @param to_match The signature to match.
+     * @return true If the signature is valid.
+     * @return false If the signature is not valid.
+     */
+    bool isValidSignature(const ComponentSignature signature, const ComponentSignature &to_match)
+    {
+        return (signature & to_match) == to_match;
+    }
 
 }
